d191c86901b92adec0a56b25c3888b3e040f0a38
SunYeop Lee <sunyeop97@gmail.com>
2016-10-02 22:36:27 +0900

    Patch for python 3

--- pcap.py
+++ pcap.py
@@ -1,4 +1,5 @@
 import struct
+import sys
 
 PCAP_GLOBAL_HEADER_FORMAT = 'IHHiIII'
 PCAP_GLOBAL_HEADER_LEN = 24
@@ -6,38 +7,46 @@ PCAP_PACKET_HEADER_FORMAT = 'IIII'
 PCAP_PACKET_HEADER_LEN = 16
 
 class PcapFile():
-    def __init__(self, filename):
-        self.f = open(filename, 'rb')
-        header_data = self.f.read(PCAP_GLOBAL_HEADER_LEN)
-        self.global_header = PcapGlobalHeader(header_data)
+	def __init__(self, filename):
+		self.filename = filename
 
-    def next_packet(self):
-        header_data = self.f.read(PCAP_PACKET_HEADER_LEN)
-        if not len(header_data):
-            return None
-        packet = PcapPacket(header_data)
-        data = self.f.read(packet.incl_len)
-        packet.data = data
-        return packet
+	def next_packet(self):
+		header_data = self.f.read(PCAP_PACKET_HEADER_LEN)
+		if not len(header_data):
+			return None
+		packet = PcapPacket(header_data)
+		data = self.f.read(packet.incl_len)
+		packet.data = data
+		return packet
+
+	def __enter__(self):
+		self.f = sys.stdin.buffer if self.filename == '-' else open(self.filename, 'rb')
+		header_data = self.f.read(PCAP_GLOBAL_HEADER_LEN)
+		self.global_header = PcapGlobalHeader(header_data)
+		return self
+
+	def __exit__(self, *args):
+		if self.filename != '-':
+			self.f.close()
 
 class PcapGlobalHeader():
-    def __init__(self, data):
-        unpacked = struct.unpack(PCAP_GLOBAL_HEADER_FORMAT, data)
-        self.magic_number = unpacked[0]
-        self.swapped = self.magic_number != 0xa1b2c3d4
-        self.version_major = unpacked[1]
-        self.version_minor = unpacked[2]
-        self.thiszone = unpacked[3]
-        self.sigfigs = unpacked[4]
-        self.snaplen = unpacked[5]
-        self.network = unpacked[6]
+	def __init__(self, data):
+		unpacked = struct.unpack(PCAP_GLOBAL_HEADER_FORMAT, data)
+		self.magic_number = unpacked[0]
+		self.swapped = self.magic_number != 0xa1b2c3d4
+		self.version_major = unpacked[1]
+		self.version_minor = unpacked[2]
+		self.thiszone = unpacked[3]
+		self.sigfigs = unpacked[4]
+		self.snaplen = unpacked[5]
+		self.network = unpacked[6]
 
 class PcapPacket():
-    def __init__(self, header_data):
-        unpacked = struct.unpack(PCAP_PACKET_HEADER_FORMAT, header_data)
-        self.ts_sec = unpacked[0]
-        self.ts_usec = unpacked[1]
-        self.incl_len = unpacked[2]
-        self.orig_len = unpacked[3]
+	def __init__(self, header_data):
+		unpacked = struct.unpack(PCAP_PACKET_HEADER_FORMAT, header_data)
+		self.ts_sec = unpacked[0]
+		self.ts_usec = unpacked[1]
+		self.incl_len = unpacked[2]
+		self.orig_len = unpacked[3]
 
 
